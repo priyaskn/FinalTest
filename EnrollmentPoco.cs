5e249b2a780cbcc0a9a814d5dfd78d728489a107
Priyadarshini <39197977+priyaskn@users.noreply.github.com>
2020-04-02 20:44:50 -0400

    Add files via upload
    Test

---
+++ EnrollmentPoco.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classroom
+{
+    public class EnrollmentPoco
+    {
+        public int EnrollmentID { get; set; }
+        public int CourseID { get; set; }
+        public int StudentID { get; set; }
+        public virtual CourcePoco Course { get; set; }
+        public virtual StudentPoco Student { get; set; }
+    }
+}
---
+++ StudentPoco.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classroom
+{
+   public class StudentPoco
+    {      
+            public int ID { get; set; }
+            public string LastName { get; set; }
+            public string FirstMidName { get; set; }
+            public DateTime EnrollmentDate { get; set; }
+
+        }
+    }
